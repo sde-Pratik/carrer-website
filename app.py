3c8d83d3165ec83fa2d337bdae99b8bf2a44dbcc
sde-Pratik <85993707+sde-Pratik@users.noreply.github.com>
2023-12-18 14:29:21 +0000

    added the flask project

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+
+app =Flask(__name__)
+
+@app.route("/") 
+def hello_world():  
+  return "hello hi"
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0',debug=True)
