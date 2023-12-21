897345e7fadc395bf0a54e944dc1bca774486bd1
sde-Pratik <85993707+sde-Pratik@users.noreply.github.com>
2023-12-21 08:27:50 +0000

    added html and css

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 
-app =Flask(__name__)
+app = Flask(__name__)
 
-@app.route("/") 
-def hello_world():  
-  return "hello hi"
+
+@app.route("/")
+def hello_world():
+  return render_template('home.html')
+
+
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0',debug=True)
+  app.run(host='0.0.0.0', debug=True)
